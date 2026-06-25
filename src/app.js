42367f39601e31b454c6170da17d26fa6015b187
kushagra garg <9429kushagragarg@gmail.com>
2026-06-25 14:34:17 +0530

    ff

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const app=express();
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 100, // limit each IP to 100 requests per windowMs
         message: "Too many requests from this IP, please try again later.",
-        validate: { xForwardedForHeader: false }
+        validations: { xForwardedForHeader: false }
     }));
     app.use(cookieParser());
     app.use(morgan('common', {
