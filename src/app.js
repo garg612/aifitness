f6e7dced1019d0031f75a72e9608a37f99348239
kushagra garg <9429kushagragarg@gmail.com>
2026-06-25 15:54:57 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -17,8 +17,7 @@ const app=express();
     app.use(rateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 100, // limit each IP to 100 requests per windowMs
-        message: "Too many requests from this IP, please try again later.",
-        validations: { xForwardedForHeader: false }
+        message: "Too many requests from this IP, please try again later."
     }));
     app.use(cookieParser());
     app.use(morgan('common', {
